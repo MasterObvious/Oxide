a981b9f70f274988d6ffee3a3811a367c0dd760c
Tim Lazarus <me@timlazarus.com>
2022-03-11 19:30:00 +0000

    Create Vulkan Instance

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::error::Error;
+use std::{error::Error, ffi::CString};
 
+use ash::{vk, Entry, Instance};
 use winit::{
     dpi::LogicalSize,
-    error::OsError,
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
+    platform::run_return::EventLoopExtRunReturn,
     window::{Window, WindowBuilder},
 };
 
@@ -12,18 +13,48 @@ const WIDTH: f64 = 800.0;
 const HEIGHT: f64 = 600.0;
 
 struct HelloTriangleApplication {
+    entry: Entry,
     event_loop: EventLoop<()>,
-    _window: Window,
+    instance: Instance,
+    window: Window,
 }
 
 impl HelloTriangleApplication {
     pub fn new() -> Result<Self, Box<dyn Error>> {
+        let entry = Entry::linked();
         let (event_loop, window) = HelloTriangleApplication::init_window()?;
 
+        let instance = HelloTriangleApplication::init_vulkan(&entry, &window)?;
+
         Ok(Self {
+            entry,
             event_loop,
-            _window: window,
+            instance,
+            window,
         })
+    }
+
+    fn init_vulkan(entry: &Entry, window: &Window) -> Result<Instance, vk::Result> {
+        let app_info = vk::ApplicationInfo::builder()
+            .application_name(CString::new("Hello Triangle").unwrap().as_c_str())
+            .application_version(vk::make_api_version(1, 0, 0, 0))
+            .engine_name(CString::new("No Engine").unwrap().as_c_str())
+            .engine_version(vk::make_api_version(1, 0, 0, 0))
+            .api_version(vk::make_api_version(1, 0, 0, 0))
+            .build();
+
+        let surface_extensions = ash_window::enumerate_required_extensions(&window).unwrap();
+        let extension_names_raw = surface_extensions
+            .iter()
+            .map(|ext| ext.as_ptr())
+            .collect::<Vec<_>>();
+
+        let instance_create_info = vk::InstanceCreateInfo::builder()
+            .application_info(&app_info)
+            .enabled_extension_names(&extension_names_raw)
+            .build();
+
+        unsafe { entry.create_instance(&instance_create_info, None) }
     }
 
     fn init_window() -> Result<(EventLoop<()>, Window), Box<dyn Error>> {
@@ -42,8 +73,8 @@ impl HelloTriangleApplication {
         Ok((event_loop, window))
     }
 
-    pub fn run(self) {
-        self.event_loop.run(move |event, _, control_flow| {
+    pub fn run(&mut self) {
+        self.event_loop.run_return(move |event, _, control_flow| {
             *control_flow = ControlFlow::Wait;
 
             if let Event::WindowEvent {
@@ -57,8 +88,15 @@ impl HelloTriangleApplication {
     }
 }
 
+impl Drop for HelloTriangleApplication {
+    fn drop(&mut self) {
+        unsafe { self.instance.destroy_instance(None) }
+        println!("We are closing the program!")
+    }
+}
+
 fn main() {
-    let triangle_app = match HelloTriangleApplication::new() {
+    let mut triangle_app = match HelloTriangleApplication::new() {
         Err(error) => panic!("Failed to create application. Cause {}", error),
         Ok(app) => app,
     };
