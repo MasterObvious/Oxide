66789f548a8e0dba7a8181a3f6a18f46d6a42227
Tim Lazarus <me@timlazarus.com>
2022-03-18 14:58:00 +0000

    Add a validation layer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use std::{error::Error, ffi::CString};
+use std::{
+    error::Error,
+    ffi::{c_void, CStr, CString},
+};
 
-use ash::{vk, Entry, Instance};
+use ash::{
+    extensions::ext::DebugUtils,
+    vk::{
+        self, DebugUtilsMessageSeverityFlagsEXT, DebugUtilsMessageTypeFlagsEXT,
+        DebugUtilsMessengerCallbackDataEXT, DebugUtilsMessengerCreateInfoEXT,
+        DebugUtilsMessengerEXT,
+    },
+    Entry, Instance,
+};
 use winit::{
     dpi::LogicalSize,
     event::{Event, WindowEvent},
@@ -12,26 +23,75 @@ use winit::{
 const WIDTH: f64 = 800.0;
 const HEIGHT: f64 = 600.0;
 
+const VALIDATION_LAYERS: [&str; 1] = ["VK_LAYER_KHRONOS_validation"];
+
+const SHOULD_INCLUDE_VALIDATION_LAYERS: bool = cfg!(debug_assertions);
+
 struct HelloTriangleApplication {
-    entry: Entry,
+    _entry: Entry,
     event_loop: EventLoop<()>,
     instance: Instance,
-    window: Window,
+    debug_utils_messenger: Option<DebugUtilsMessengerEXT>,
+    _window: Window,
 }
 
 impl HelloTriangleApplication {
     pub fn new() -> Result<Self, Box<dyn Error>> {
         let entry = Entry::linked();
-        let (event_loop, window) = HelloTriangleApplication::init_window()?;
+        let (event_loop, window) = Self::init_window()?;
 
-        let instance = HelloTriangleApplication::init_vulkan(&entry, &window)?;
+        let instance = Self::init_vulkan(&entry, &window)?;
+
+        let debug_utils_messenger = if SHOULD_INCLUDE_VALIDATION_LAYERS {
+            Some(Self::init_debug_messenger(&entry, &instance))
+        } else {
+            None
+        };
 
         Ok(Self {
-            entry,
+            _entry: entry,
             event_loop,
             instance,
-            window,
+            debug_utils_messenger,
+            _window: window,
         })
+    }
+
+    unsafe extern "system" fn vulkan_debug_callback(
+        _message_severity: DebugUtilsMessageSeverityFlagsEXT,
+        _message_types: DebugUtilsMessageTypeFlagsEXT,
+        p_callback_data: *const DebugUtilsMessengerCallbackDataEXT,
+        _p_data: *mut c_void,
+    ) -> vk::Bool32 {
+        let message_pointer = (*p_callback_data).p_message;
+        let message = CStr::from_ptr(message_pointer);
+        println!("Validation layer: {:?}", message);
+
+        vk::FALSE
+    }
+
+    fn create_debug_messenger_create_info() -> DebugUtilsMessengerCreateInfoEXT {
+        vk::DebugUtilsMessengerCreateInfoEXT::builder()
+            .message_severity(
+                vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+            )
+            .message_type(
+                vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                    | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE
+                    | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION,
+            )
+            .pfn_user_callback(Some(Self::vulkan_debug_callback))
+            .build()
+    }
+
+    fn init_debug_messenger(entry: &Entry, instance: &Instance) -> DebugUtilsMessengerEXT {
+        let debug_messenger_info = Self::create_debug_messenger_create_info();
+
+        let debug_utils_loader = DebugUtils::new(entry, instance);
+        unsafe { debug_utils_loader.create_debug_utils_messenger(&debug_messenger_info, None) }
+            .unwrap()
     }
 
     fn init_vulkan(entry: &Entry, window: &Window) -> Result<Instance, vk::Result> {
@@ -43,16 +103,44 @@ impl HelloTriangleApplication {
             .api_version(vk::make_api_version(1, 0, 0, 0))
             .build();
 
-        let surface_extensions = ash_window::enumerate_required_extensions(&window).unwrap();
+        let mut surface_extensions = ash_window::enumerate_required_extensions(&window).unwrap();
+
+        if SHOULD_INCLUDE_VALIDATION_LAYERS {
+            surface_extensions.push(ash::extensions::ext::DebugUtils::name())
+        }
+
         let extension_names_raw = surface_extensions
             .iter()
             .map(|ext| ext.as_ptr())
             .collect::<Vec<_>>();
 
-        let instance_create_info = vk::InstanceCreateInfo::builder()
+        let layer_names = if SHOULD_INCLUDE_VALIDATION_LAYERS {
+            VALIDATION_LAYERS
+                .iter()
+                .filter_map(|string| CString::new(*string).ok())
+                .collect::<Vec<_>>()
+        } else {
+            vec![]
+        };
+
+        let layer_names_raw = layer_names
+            .iter()
+            .map(|ext| ext.as_ptr())
+            .collect::<Vec<_>>();
+
+        let instance_create_info_builder = vk::InstanceCreateInfo::builder()
             .application_info(&app_info)
             .enabled_extension_names(&extension_names_raw)
-            .build();
+            .enabled_layer_names(&layer_names_raw);
+
+        let mut debug_messenger_create_info = Self::create_debug_messenger_create_info();
+
+        let instance_create_info = match SHOULD_INCLUDE_VALIDATION_LAYERS {
+            true => instance_create_info_builder
+                .push_next(&mut debug_messenger_create_info)
+                .build(),
+            false => instance_create_info_builder.build(),
+        };
 
         unsafe { entry.create_instance(&instance_create_info, None) }
     }
@@ -65,12 +153,10 @@ impl HelloTriangleApplication {
             .with_inner_size(LogicalSize::new(WIDTH, HEIGHT))
             .build(&event_loop);
 
-        let window = match window_result {
-            Ok(w) => w,
-            Err(e) => return Err(Box::new(e)),
-        };
-
-        Ok((event_loop, window))
+        match window_result {
+            Ok(w) => Ok((event_loop, w)),
+            Err(e) => Err(Box::new(e)),
+        }
     }
 
     pub fn run(&mut self) {
@@ -90,6 +176,13 @@ impl HelloTriangleApplication {
 
 impl Drop for HelloTriangleApplication {
     fn drop(&mut self) {
+        let debug_utils_loader = DebugUtils::new(&self._entry, &self.instance);
+        if SHOULD_INCLUDE_VALIDATION_LAYERS {
+            unsafe {
+                debug_utils_loader
+                    .destroy_debug_utils_messenger(self.debug_utils_messenger.unwrap(), None)
+            }
+        }
         unsafe { self.instance.destroy_instance(None) }
         println!("We are closing the program!")
     }
